f2ad780ff7469a9bb383df91ee53f7c197350447
sam <1102566608@qq.com>
2024-08-07 15:09:32 +0800

    第一次可以用版本

--- app.py
+++ app.py
@@ -11,21 +11,52 @@ def read_config():
     config.read(CONFIG_PATH, encoding='utf-8')
     return config
 
+def read_common_section():
+    with open(CONFIG_PATH, 'r') as file:
+        lines = file.readlines()
+        common_section = ''
+        common_found = False
+        common_lines = []
+        other_lines = []
+        for line in lines:
+            if line.strip() == '[common]':
+                common_found = True
+            elif common_found and line.strip().startswith('['):
+                common_found = False
+            if common_found:
+                common_lines.append(line)
+            else:
+                other_lines.append(line)
+        common_section = ''.join(common_lines[1:]).rstrip()  # 去除"[common]"和最后的空行
+        return common_section, other_lines
+
 def write_config(config):
+    common_config, _ = read_common_section()
     with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+        file.write('[common]\n' + common_config + '\n\n')
         for section in config.sections():
-            file.write(f'[{section}]\n')
-            for key in config[section]:
-                if config[section][key] is None or config[section][key].strip() == '':
-                    file.write(f'# {key} =\n')
-                else:
-                    file.write(f'{key} = {config[section][key]}\n')
-            file.write('\n')
+            if section != 'common':
+                file.write(f'[{section}]\n')
+                for key in config[section]:
+                    if config[section][key] is None or config[section][key].strip() == '':
+                        file.write(f'# {key} =\n')
+                    else:
+                        file.write(f'{key} = {config[section][key]}\n')
+                file.write('\n')
+
+
+def write_common_section(common_config):
+    _, other_lines = read_common_section()
+    with open(CONFIG_PATH, 'w', encoding='utf-8') as file:
+        file.write('[common]\n' + common_config + '\n')
+        for line in other_lines:
+            file.write(line)
 
 @app.route('/')
 def index():
     config = read_config()
-    return render_template('index.html', config=config)
+    common_section, _ = read_common_section()
+    return render_template('index.html', config=config, common_config=common_section)
 
 @app.route('/update/<section>', methods=['POST'])
 def update(section):
@@ -42,22 +73,10 @@ def update(section):
         write_config(config)
     return redirect(url_for('index'))
 
-@app.route('/add_option/<section>', methods=['POST'])
-def add_option(section):
-    config = read_config()
-    if section in config:
-        new_option = request.form['new_option']
-        if new_option and new_option not in config[section]:
-            config[section][new_option] = ''
-        write_config(config)
-    return redirect(url_for('index'))
-
-@app.route('/delete_option/<section>/<option>', methods=['POST'])
-def delete_option(section, option):
-    config = read_config()
-    if section in config and option in config[section]:
-        del config[section][option]
-        write_config(config)
+@app.route('/update_common', methods=['POST'])
+def update_common():
+    common_config = request.form['common_config']
+    write_common_section(common_config)
     return redirect(url_for('index'))
 
 @app.route('/add', methods=['POST'])
